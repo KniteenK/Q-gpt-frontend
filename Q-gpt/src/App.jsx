799b229776727e781188a122c8049cc5233221c8
Niketiiitd <niket22320@iiitd.ac.in>
2024-07-18 00:40:20 +0530

    Homepage added
    Features added ->
    1) login/signup
    2) heading of webpage
    3) browse file that include upload and cancel button.Additional featiures added -> alert if not is json or csv format

--- Q-gpt/src/App.jsx
+++ Q-gpt/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import Homepage from './components/Homepage';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './index.css';
 
 function App() {
   // console.log("Hello");
@@ -9,6 +10,8 @@ function App() {
     <Router>
         <Routes>
           <Route path="/" element={<Homepage />} />
+          {/* <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} /> */}
         </Routes>
     </Router>
   );
